ca2005ddd6cf11316013f89de87ec6426c86270a
Akshat <akshatgauriiitsurat@gmail.com>
2022-07-16 15:10:01 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import "./App.css";
 import styled from "styled-components";
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
-import { Earth } from './components/earth/Earth';
-
+import { Earth } from "./components/earth/Earth";
+import { Content } from "./components/content";
+import { Stars } from "@react-three/drei";
 
 function App() {
   const Container = styled.div`
@@ -11,15 +12,22 @@ function App() {
     height: 100vh;
   `;
   return (
-    <div className="app">
-      <Container>
-        <Canvas camera={{position:[48, 10, 50]}}>
-          <Suspense fallback={null}>
-            <Earth />
-          </Suspense>
-        </Canvas>
-      </Container>
-    </div>
+    <>
+      <Canvas camera={{position:[0, 0, 0]}}>
+        <Suspense fallback={null}>
+          <Earth />
+          <Stars
+            radius={100}
+            depth={60}
+            count={20000}
+            factor={10}
+            saturation={0}
+            fade={true}
+          />
+        </Suspense>
+      </Canvas>
+      <Content />
+    </>
   );
 }
 
--- src/components/earth/Earth.jsx
+++ src/components/earth/Earth.jsx
@@ -1,97 +1,72 @@
-import React, { useRef } from "react";
 import { useFrame, useLoader } from "@react-three/fiber";
-import { OrbitControls, Stars } from "@react-three/drei";
-import * as THREE from "three";
+import React, { useRef } from "react";
 
+import MoonMap from "../../assets/textures/4k_ceres_fictional.jpg";
+import EarthCloudsMap from "../../assets/textures/8k_earth_clouds.jpg";
 import EarthDayMap from "../../assets/textures/8k_earth_daymap.jpg";
 import EarthNormalMap from "../../assets/textures/8k_earth_normal_map.jpg";
 import EarthSpecularMap from "../../assets/textures/8k_earth_specular_map.jpg";
-import EarthCloudsMap from "../../assets/textures/8k_earth_clouds.jpg";
-import MoonMap from "../../assets/textures/4k_ceres_fictional.jpg";
 import SunMap from "../../assets/textures/8k_sun.jpg";
+import MeMap from "../../assets/textures/me.jpg";
+import SpaceMap from "../../assets/textures/space.jpg";
+
 import { TextureLoader } from "three";
-
 export function Earth(props) {
-  const [colorMap, normalMap, specularMap, cloudsMap, moonMap, sunMap] = useLoader(
-    TextureLoader,
-    [EarthDayMap, EarthNormalMap, EarthSpecularMap, EarthCloudsMap, MoonMap, SunMap]
-  );
+  const [
+    colorMap,
+    normalMap,
+    specularMap,
+    cloudsMap,
+    moonMap,
+    sunMap,
+    meMap,
+    spaceMap,
+  ] = useLoader(TextureLoader, [
+    EarthDayMap,
+    EarthNormalMap,
+    EarthSpecularMap,
+    EarthCloudsMap,
+    MoonMap,
+    SunMap,
+    MeMap,
+    SpaceMap,
+  ]);
 
-  const earthRef = useRef();
-  const cloudsRef = useRef();
   const moonRef = useRef();
   const sunRef = useRef();
+  const meRef = useRef();
 
-  useFrame(({ clock }) => {
+  useFrame(({ clock, scene, gl, camera }) => {
     const elapsedTime = clock.getElapsedTime();
-
-    earthRef.current.rotation.y = elapsedTime / 6;
-    cloudsRef.current.rotation.y = elapsedTime / 6;
     moonRef.current.rotation.x = moonRef.current.rotation.y += 0.01;
     sunRef.current.rotation.x = sunRef.current.rotation.y -= 0.01;
+    meRef.current.rotation.x = meRef.current.rotation.y -= 0.01;
+    scene.background = spaceMap;
+    moonRef.current.position.x = -10;
+    moonRef.current.position.z = 30;
+    meRef.current.position.set(0, 0, -7);
+    window.addEventListener("scroll", () => {
+      const t = document.body.getBoundingClientRect().top;
+      camera.position.z = t * -0.01;
+      camera.position.x = t * -0.0002;
+      camera.rotation.y = t * -0.0002;
+    });
   });
-
   return (
     <>
-      {/* <ambientLight intensity={1} /> */}
-      <pointLight color="#f6f3ea" position={[2, 0, 5]} intensity={1.2} />
-      <Stars
-        radius={300}
-        depth={60}
-        count={20000}
-        factor={7}
-        saturation={0}
-        fade={true}
-      />
-      <mesh ref={cloudsRef} position={[0, 0, 3]}>
+      <pointLight color="#f6f3ea" position={[50, 10, 3]} intensity={1.2} />
+      <directionalLight position={[50, 0, 3]} />
+      <mesh visible castShadow rotation={[0, 0, 0]} ref={moonRef}>
         <torusGeometry args={[2, 1, 64, 100]} />
-        <meshPhongMaterial
-          map={cloudsMap}
-          opacity={0.4}
-          depthWrite={true}
-          transparent={true}
-          side={THREE.DoubleSide}
-        />
-      </mesh>
-      <mesh ref={earthRef} position={[0, 0, 3]}>
-        <torusGeometry args={[2, 1, 64, 100]} />
-        <meshPhongMaterial specularMap={specularMap} />
-        <meshStandardMaterial
-          map={colorMap}
-          normalMap={normalMap}
-          metalness={0.6}
-          roughness={0.9}
-        />
-        <OrbitControls
-          enableZoom={true}
-          enablePan={true}
-          enableRotate={true}
-          zoomSpeed={0.6}
-          panSpeed={0.5}
-          rotateSpeed={0.4}
-        />
-      </mesh>
-      <ambientLight intensity={0.1} />
-      <directionalLight position={[10, 0, 3]}/>
-      <mesh
-        vivble
-        castShadow
-        rotation={[0, 0, 0]}
-        ref={moonRef}
-        position={[0, 0, 3]}
-      >
-        <torusGeometry args={[6, 3, 64, 100]} />
         <meshStandardMaterial metalness={1} map={moonMap} color="white" />
       </mesh>
-      <mesh
-        vivble
-        castShadow
-        rotation={[0, 0, 0]}
-        ref={sunRef}
-        position={[0, 0, 3]}
-      >
-        <torusGeometry args={[18, 9, 64, 100]} />
+      <mesh visible castShadow rotation={[0, 0, 0]} ref={sunRef}>
+        <torusGeometry args={[6, 3, 64, 100]} />
         <meshStandardMaterial metalness={1} map={sunMap} color="white" />
+      </mesh>
+      <mesh ref={meRef}>
+        <boxGeometry args={[3, 3, 3]} />
+        <meshStandardMaterial metalness={0} map={meMap} />
       </mesh>
     </>
   );
